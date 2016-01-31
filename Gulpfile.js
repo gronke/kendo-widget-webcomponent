0b9d943e9486daf28bfe12820062e59820ef2388
Stefan Grönke <stefan@gronke.net>
2016-01-31 13:23:05 +0100

    initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,64 @@
+var path = require('path'),
+  fs = require('fs'),
+  gulp = require('gulp'),
+  rmdir = require('rmdir'),
+  mkdirp = require('mkdirp'),
+  merge = require('merge2'),
+  runSequence = require('run-sequence'),
+  gulpLoadPlugins = require('gulp-load-plugins');
+
+var plugins = gulpLoadPlugins();
+
+var config = {
+  src: './src',
+  dist: './dist',
+  test: './test'
+};
+
+gulp.task('clean', function() {
+  return new Promise(function(resolve, reject) {
+    function done() {
+      mkdirp(config.dist);
+      resolve();
+    }
+    try {
+      if(fs.existsSync(config.dist)) {
+        rmdir(config.dist, done);
+      } else {
+        done();
+      }
+    } catch(e) {
+      reject(e);
+    }
+  });
+});
+
+gulp.task('typescript', function() {
+
+  var tsResult = gulp.src(path.join(config.src, 'kendo-widget-webcomponent.ts'))
+  .pipe(plugins.typescript({
+    declaration: true
+  }));
+
+  return tsResult.js.pipe(gulp.dest(config.dist));
+
+});
+
+gulp.task('mocha', function() {
+  return gulp.src(path.join(config.test, 'runner.html'), {
+    read: false
+  }).pipe(plugins.mochaPhantomjs({
+    reporter: 'spec',
+    phantomjs: {
+      useColors: true
+    }
+  }));
+});
+
+gulp.task('test', function(cb) {
+  return runSequence('build', 'mocha', cb);
+});
+gulp.task('build', function(cb) {
+  return runSequence('clean', 'typescript', cb);
+});
+gulp.task('default', ['build']);
---
+++ README.md
@@ -0,0 +1,31 @@
+Kendo Widget Webcomponent
+=========================
+Use HTML5 Imports to load Kendo UI widgets.
+
+Background
+----------
+Larger applications tend to have a large list of assets that are actually not used by the application. HTML5 imports provide the ability to load components of an application asynchronously via import tag and can also handle the comoponent registration at the main application.
+
+Install
+-------
+```bash
+bower install gronke/kendo-widget-webcomponent
+```
+
+Load the helper before your main application
+```html
+<script type="text/javascript" src="bower_components/kendo-widget-webcomponent/dist/kendo-widget-webcomponent.js"></script>
+```
+
+Development
+-----------
+```bash
+npm install --only=dev
+bower install
+```
+
+### Run Unit Tests
+Mocha unit tests can be run via Gulp test task
+```bash
+gulp test
+```
